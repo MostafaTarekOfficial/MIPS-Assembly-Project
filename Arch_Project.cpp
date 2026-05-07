e415b651594e47b4cc3ac2095e223565d1582b6a
Mostafa_Tarek <mostafatarek6227@gmail.com>
2026-05-08 00:32:52 +0300

    Update Arch_Project.cpp

--- Arch_Project.cpp
+++ Arch_Project.cpp
@@ -15,6 +15,7 @@ int main()
 	int num1;
 	int num2;
 	int num3;
+	int choice;
 	
 		cout<<"Enter The First Number : ";
 		cin>>num1;
@@ -25,9 +26,13 @@ int main()
 		cout<<"Enter The Third Number : ";
 		cin>>num3;
 	
-		cout<<"sum (N1+N2+N3) = "<<num1+num2+num3<<'\n';
-	
-		cout<<"sub (N1-N2-N3) = "<<num1-num2-num3;
-	
+		cout << "Enter 1 for sum or 2 for sub: "; 
+		cin >> choice;      
+
+	if (choice == 1)         
+			cout << "sum (N1+N2+N3) = " << num1+num2+num3;     
+    else         
+			cout << "sub (N1-N2-N3) = " << num1-num2-num3;
+
 	return 0;
 }
